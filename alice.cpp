6ede55cd4a1cefa7846088365e76d7633b593abd
JialongYin <yinjlusa@gmail.com>
2022-03-05 18:14:22 +0800

    m

--- alice.cpp
+++ alice.cpp
@@ -215,6 +215,11 @@ const Message *recv()
     }
     static Message *m = (Message *)malloc(MESSAGE_SIZES[4]);
     std::cout << "alice recv 1" << std::endl;
+    
+    int restrict sval;
+    sem_getvalue(full_ba, &sval);
+    std::cout << sval << std::endl;
+
     sem_wait(full_ba);
     sem_wait(mutex_ba);
     std::cout << "alice recv 2" << std::endl;
