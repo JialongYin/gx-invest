207fc4801c29a145f490331820cc0e3d94080b54
JialongYin <yinjlusa@gmail.com>
2022-03-06 16:55:25 +0800

    named pipe non blocking version

--- alice.cpp
+++ alice.cpp
@@ -257,6 +257,10 @@ const Message *recv()
 
 int main()
 {
+
+    /*Non-blocking FIFO*/
+    const Message *m1;
+
     while (true)
     {
 
@@ -264,10 +268,7 @@ int main()
         // const Message *m1 = next_message();
         /*Non-blocking FIFO*/
         if (bytew != -1) {
-            const Message *m1 = next_message();
-        } else {
-            const Message *m1 = nullptr;
-        }
+            m1 = next_message();
 
         if (m1)
         {
